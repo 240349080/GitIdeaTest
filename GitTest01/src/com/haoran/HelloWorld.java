8fc6a74ded0a0adb28ab71a203d62b2fb938f83b
guohaoran <240349080@qq.com>
2020-02-23 18:58:02 +0800

    测试

---
+++ GitTest01/src/com/haoran/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.haoran;
+
+/**
+ * @author guohaoran
+ * @create 2020/2/23 18:55
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
